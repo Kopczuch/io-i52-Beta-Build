ddb8cc63d68372c79ae81702502de6668e2b5c2f
Mikołaj Krakowiak <79112577+Slo1k@users.noreply.github.com>
2022-12-10 14:18:05 +0500

    Add classes representing locations

---
+++ src/main/java/pl/put/poznan/transformer/logic/Floor.java
@@ -0,0 +1,58 @@
+package pl.put.poznan.transformer.logic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Implementation of the abstract class Floor
+ *
+ * @author Mikołaj Krakowiak
+ * @version 1.0
+ */
+
+public class Floor extends Location{
+
+    private List<Room> rooms = new ArrayList<Room>();
+    private int id;
+    private String name;
+
+
+    public Floor(int id, String name){
+        super(id, name);
+    }
+
+    public List<Room> getRooms(){
+        return rooms;
+    }
+
+    public void addRoom(Room r){
+        rooms.add(r);
+    }
+
+    @Override
+    public double calculateArea(){
+        double areaSum = 0;
+        for (Room r: rooms){
+            areaSum += r.getArea();
+        }
+        return areaSum;
+    }
+
+    @Override
+    public double calculateVolume(){
+        double volumeSum = 0;
+        for (Room r: rooms){
+            volumeSum += r.getVolume();
+        }
+        return volumeSum;
+    }
+
+    @Override
+    public double calculateLightPower() {
+        double sumLightPower = 0;
+        for (Room r: rooms){
+            sumLightPower += r.calculateLightPower();
+        }
+        return sumLightPower;
+    }
+}
