69503c0f6a2529c0d0c04d6732408c5fe954e3aa
Adam Kopiec <adas073@gmail.com>
2022-12-12 15:52:18 +0100

    new files names

--- src/main/java/pl/put/poznan/buildinfo/app/TextTransformerApplication.java
+++ src/main/java/pl/put/poznan/buildinfo/app/TextTransformerApplication.java
@@ -1,13 +1,17 @@
-package pl.put.poznan.transformer.app;
+package pl.put.poznan.buildinfo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.transformer.rest"})
+@RestController
 public class TextTransformerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TextTransformerApplication.class, args);
     }
+
 }
---
+++ src/main/java/pl/put/poznan/buildinfo/logic/Location.java
@@ -0,0 +1,41 @@
+package pl.put.poznan.buildinfo.logic;
+
+/*
+* Implementation of the abstract class Location
+*
+* @author Mikołaj Krakowiak
+* @version 1.0
+ */
+
+public abstract class Location {
+
+    private int id;
+    private String name;
+
+    public Location(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract double calculateArea();
+
+    public abstract double calculateVolume();
+
+    public abstract double calculateLightPower();
+}
